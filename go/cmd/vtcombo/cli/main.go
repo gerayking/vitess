dd8deec393e1ddd51ed0d1d2b4a4fad824371d02
Arthur Schreiber <arthurschreiber@github.com>
2024-01-15 11:56:41 +0000

    Allow specifying the tablet types to wait for in vtcombo.
    Signed-off-by: Arthur Schreiber <arthurschreiber@github.com>

--- go/cmd/vtcombo/cli/main.go
+++ go/cmd/vtcombo/cli/main.go
@@ -42,6 +42,7 @@ import (
 	"vitess.io/vitess/go/vt/srvtopo"
 	"vitess.io/vitess/go/vt/topo"
 	"vitess.io/vitess/go/vt/topo/memorytopo"
+	"vitess.io/vitess/go/vt/topo/topoproto"
 	"vitess.io/vitess/go/vt/topotools"
 	"vitess.io/vitess/go/vt/vtcombo"
 	"vitess.io/vitess/go/vt/vtctld"
@@ -78,12 +79,13 @@ In particular, it contains:
 	plannerName           string
 	vschemaPersistenceDir string
 
-	tpb             vttestpb.VTTestTopology
-	ts              *topo.Server
-	collationEnv    *collations.Environment
-	resilientServer *srvtopo.ResilientServer
-	parser          *sqlparser.Parser
-	mysqlVersion    string
+	tpb               vttestpb.VTTestTopology
+	ts                *topo.Server
+	collationEnv      *collations.Environment
+	resilientServer   *srvtopo.ResilientServer
+	parser            *sqlparser.Parser
+	mysqlVersion      string
+	tabletTypesToWait []topodatapb.TabletType
 )
 
 func init() {
@@ -115,6 +117,8 @@ func init() {
 
 	Main.Flags().Var(vttest.TextTopoData(&tpb), "proto_topo", "vttest proto definition of the topology, encoded in compact text format. See vttest.proto for more information.")
 	Main.Flags().Var(vttest.JSONTopoData(&tpb), "json_topo", "vttest proto definition of the topology, encoded in json format. See vttest.proto for more information.")
+
+	Main.Flags().Var((*topoproto.TabletTypeListFlag)(&tabletTypesToWait), "tablet_types_to_wait", "Wait till connected for specified tablet types during Gateway initialization. Should be provided as a comma-separated set of tablet types.")
 
 	// We're going to force the value later, so don't even bother letting the
 	// user know about this flag.
@@ -298,11 +302,22 @@ func run(cmd *cobra.Command, args []string) (err error) {
 
 	// vtgate configuration and init
 	resilientServer = srvtopo.NewResilientServer(context.Background(), ts, "ResilientSrvTopoServer")
-	tabletTypesToWait := []topodatapb.TabletType{
-		topodatapb.TabletType_PRIMARY,
-		topodatapb.TabletType_REPLICA,
-		topodatapb.TabletType_RDONLY,
+
+	tabletTypes := make([]topodatapb.TabletType, 0, 1)
+	if len(tabletTypesToWait) != 0 {
+		for _, tt := range tabletTypesToWait {
+			if topoproto.IsServingType(tt) {
+				tabletTypes = append(tabletTypes, tt)
+			}
+		}
+
+		if len(tabletTypes) == 0 {
+			log.Exitf("tablet_types_to_wait should contain at least one serving tablet type")
+		}
+	} else {
+		tabletTypes = append(tabletTypes, topodatapb.TabletType_PRIMARY, topodatapb.TabletType_REPLICA, topodatapb.TabletType_RDONLY)
 	}
+
 	plannerVersion, _ := plancontext.PlannerNameToVersion(plannerName)
 
 	vtgate.QueryLogHandler = "/debug/vtgate/querylog"
@@ -310,7 +325,7 @@ func run(cmd *cobra.Command, args []string) (err error) {
 	vtgate.QueryzHandler = "/debug/vtgate/queryz"
 
 	// pass nil for healthcheck, it will get created
-	vtg := vtgate.Init(context.Background(), nil, resilientServer, tpb.Cells[0], tabletTypesToWait, plannerVersion, collationEnv, mysqlVersion)
+	vtg := vtgate.Init(context.Background(), nil, resilientServer, tpb.Cells[0], tabletTypes, plannerVersion, collationEnv, mysqlVersion)
 
 	// vtctld configuration and init
 	err = vtctld.InitVtctld(ts, collationEnv, parser, mysqlVersion)
